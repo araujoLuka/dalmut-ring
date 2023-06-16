5d17f353f5f26e73bdf2b515dc12490d67fb43c1
loi777 <35152365+loi777@users.noreply.github.com>
2023-06-16 04:12:55 -0300

    Update cartas.h

--- SourceCode/cartas.h
+++ SourceCode/cartas.h
@@ -1,3 +1,10 @@
-typedef struct hand {
-    int cards[13];      // 13 slots para cada tipo de carta que se pode ter
-} hand;
+typedef struct deck {
+    int cartas[13];      // 13 slots para cada tipo de carta que se pode ter
+} deck;
+
+
+
+
+// verifica se o jogador ainda possui cartas em maos
+// retorna 1 se sim, e 0 se nao
+int verificar_cartas();
