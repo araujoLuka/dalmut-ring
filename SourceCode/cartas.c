2520c422f20cd6e7f3beeb7ed2eac7d5827d95de
loi777 <35152365+loi777@users.noreply.github.com>
2023-06-16 18:47:24 -0300

    Update cartas.c

--- SourceCode/cartas.c
+++ SourceCode/cartas.c
@@ -11,6 +11,8 @@
 
 deck cartas;
 
+extern maquina computador;
+
 extern estado_jogo jogo;
 
 
@@ -28,38 +30,21 @@ void prepara_deck() {
     }
 
     // coloca 2 coringas
-    cartas.cartas[13] = 2;
+    cartas.cartas[12] = 2;
 }
 
 
-
-//-----------------------------------------------------------------
-
-
-
-// aleatoriamente da as cartas para as outras maquinas
-void dar_cartas() {
-    // somente a primeira maquina pode ser o carteador
-    if (jogo.bastao == 0) {
-        return;
+// limpa o deck para ser usado
+void limpa_deck() {
+    // esvazia o deck
+    for (int i = 0; i < 13; i++) {
+        cartas.cartas[i] = 0;
     }
-
-    //
-
-    prepara_deck();
-    srand(time(NULL));
-
-    //
-
-    // elaborar como distribuir as cartas 1 por 1 do id 1 adiante, ate nao sobrar mais no deck
-
-    //
 }
 
 
 
 //-----------------------------------------------------------------
-
 
 
 
@@ -73,4 +58,191 @@ int verificar_cartas() {
     }
 
     return 1;
+}
+
+
+
+//-----------------------------------------------------------------
+
+
+
+// desenha no stdout as cartas do jogador em ordem crescente
+// melhor --> pior
+void printa_cartas() {
+    printf("\nSuas cartas atuais:\n\n");
+
+    //
+
+    for(int i = 0; i < 12; i++) {
+        if (cartas.cartas[i] > 0) {
+            printf("[%d] quantidade: %d\n", i, cartas.cartas[i]);
+        }
+    }
+
+    //
+
+    if (cartas.cartas[12] > 0) {
+        printf("[Coringa] quantidade: %d\n", cartas.cartas[12]);
+    }
+} 
+
+
+
+//-----------------------------------------------------------------
+
+
+
+// faz os varios testes para poder jogar as cartas
+// envia a mensagem das cartas jogadas
+// testa se ganhou
+// passa o bastao pro proximo
+void jogar_cartas() {
+    int nivel = 0;
+    int quantidade = 0;
+    int coringas = 0;
+
+    //
+
+    printf("Escolha um nivel (valor errado cancela)\n");
+    scanf("%d", &nivel);
+
+    //
+
+    if (nivel <= 0 || nivel > 13) {
+        printf("Nivel escolhido invalido, precisa ser entre 1 e 13\n");
+        return;
+    }
+
+    if (cartas.cartas[nivel] == 0) {
+        printf("Voce nao possui cartas desse nivel\n");
+        return;
+    }
+    
+    if (nivel > jogo.lastPlayed_nivel) {
+        printf("Nivel escolhido eh maior que o ultimo nivel jogado\n");
+        return;
+    }
+
+    //
+    
+    printf("Quantas cartas?\n");
+    scanf("%d", &quantidade);
+
+    //
+
+    if (quantidade != jogo.lastPlayed_quantidade && jogo.lastPlayed_quantidade > 0) {
+        printf("Quantidade escolhida nao eh igual a quantidade jogada anteriormente\n");
+        return;
+    }
+
+    if (quantidade > cartas.cartas[nivel]) {
+        printf("Voce nao tem tantas cartas nesse nivel\n");
+        return;
+    }
+
+    if (quantidade <= 0) {
+        printf("Quantidade invalida, precisa ser maior que 0\n");
+        return;
+    }
+
+    //
+
+    if (cartas.cartas[12] > 0 && nivel != 12) {
+        printf("Quantos coringas deseja usar? (0 para nao usar nenhum)\n");
+        scanf("%d", &coringas);
+
+        //
+
+        if (coringas < 0) {
+            printf("Quantidade invalida, nao coloque valores negativos\n");
+            return;
+        }
+
+        if (coringas > cartas.cartas[12]) {
+            printf("Voce nao tem tantos coringas\n");
+            return;
+        }
+    }
+
+    //
+
+    printf("\nJogada bem sucedida\n");
+    printf("Jogando %dx cartas de nivel %d\n", quantidade, nivel);
+    if (coringas > 0) {
+        printf("junto com %dx coringas\n", coringas);
+    }
+
+    //
+
+    cartas.cartas[nivel] -= quantidade;
+    cartas.cartas[12] -= coringas;
+
+    //
+
+    int conteudo = (nivel) | (quantidade + coringas << 8);
+    // primeiro byte eh o tipo
+    // segundo byte a quantidade
+
+    enviar_mensagem((char) MEN_JOGADA, computador.id, (1 << computador.id), conteudo);
+    protocolo_de_tratamento();
+
+    //
+
+    // apos todos estarem cientes da jogada feita
+    // verifica se esse jogador venceu nessa jogada
+    verifica_vitoria();
+    passar_bastao(computador.id_next);
+}
+
+
+
+//-----------------------------------------------------------------
+
+
+
+// aleatoriamente da as cartas para as outras maquinas
+// 1 por 1 em ordem 'horaria'
+void dar_cartas() {
+    int cartas_a_dar = 80;
+    int id_prox_a_ganhar_carta = 0;
+
+    //
+
+    srand(time(NULL));
+
+    //
+
+    int nivel, conteudo;
+
+    //
+
+    while(cartas_a_dar > 0) {
+        if (id_prox_a_ganhar_carta != computador.id) {
+
+            // gera um nivel de carta valido
+            nivel = rand() % 13;
+    
+            // caso nao tenha mais essa carta
+            // pula pra uma pior e re-testa
+            while (cartas.cartas[nivel] == 0) {
+                nivel = (nivel + 1) % 13;
+            }
+    
+            //
+    
+            conteudo = (nivel) | (id_prox_a_ganhar_carta << 8);
+    
+            //
+            
+            cartas.cartas[nivel] --;
+            enviar_mensagem((char) MEN_COMPRANDO_CARTA, computador.id, (1 << computador.id), conteudo);
+            protocolo_de_tratamento();
+
+        }
+
+        //
+
+        id_prox_a_ganhar_carta = (id_prox_a_ganhar_carta + 1) % computador.qtd_maquinas;
+        cartas_a_dar --;
+    }
 }
