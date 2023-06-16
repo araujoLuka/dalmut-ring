74a80891e811905b711300a3043d07220b3fc9ea
loi777 <35152365+loi777@users.noreply.github.com>
2023-06-16 18:48:32 -0300

    Update maquinas.h

--- SourceCode/maquinas.h
+++ SourceCode/maquinas.h
@@ -1,14 +1,22 @@
 typedef struct maquina {
-    int qtd_maquinas;   // de setup.txt, obtem a quantidade de maquinas
-    char** todos_ips;   // de setup.txt, obtem todos os ips em ordem
+    int qtd_maquinas;           // de setup.txt, obtem a quantidade de maquinas
+    char** todos_ips;           // de setup.txt, obtem todos os ips em ordem
 
-    char* ip;           // ip dessa maquina
-    int id;             // id dessa maquina no arquivo setup.txt
+    int confirmacao_completa;   // salva o valor de todos as maquinas confirmarem
+    // simplifica um pouco o codigo e impede calcular td hr isso
 
-    int socket_next;    // o socket aberto para a maquina posterios
-    int socket_prev;    // o socket aberto para a maquina anterior
+    char* ip;                   // ip dessa maquina
+    int id;                     // id dessa maquina no arquivo setup.txt
 
-    int id_next;      // o id da maquina posterior
-    int id_prev;      // o id da maquina anterior
+    int socket_next;            // o socket aberto para a maquina posterios
+    int socket_prev;            // o socket aberto para a maquina anterior
+
+    int id_next;                // o id da maquina posterior
+    int id_prev;                // o id da maquina anterior
     // os ips podem ser extrapolados da lista de ips
 } maquina;
+
+
+
+
+void encerra_coneccoes();
