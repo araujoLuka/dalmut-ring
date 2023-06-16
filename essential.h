51e2e25a1650bbb9d47985457ab0b4cb91d01cf4
loi777 <35152365+loi777@users.noreply.github.com>
2023-06-16 20:24:05 -0300

    Update essential.h

--- essential.h
+++ essential.h
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <errno.h>
+#include <arpa/inet.h>
 
 #include "./SourceCode/cartas.h"
 #include "./SourceCode/jogo.h"
@@ -7,3 +12,6 @@
 
 
 //#define DEBUGG
+
+
+#define PORT 4249
