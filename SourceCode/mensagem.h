0ddb080ed7055369f1d5bee6b682d20a3eb2a747
loi777 <35152365+loi777@users.noreply.github.com>
2023-06-16 20:22:09 -0300

    Update mensagem.h

--- SourceCode/mensagem.h
+++ SourceCode/mensagem.h
@@ -1,10 +1,8 @@
 typedef struct mensagem {
 
-    int conteudo;       // 2 bytes, nao sei como isso deve ficar
-
-    // para jogadas ou dando cartas
-    // 1 byte sera para o tipo de carta
-    // 2 byte para a quantidade         (255 max, mas so usaremos, 14(0000 1110) no maximo do maximo)
+    char conteudo1;       // 1 byte de conteudo
+    char conteudo2;       // 1 byte de conteudo 2
+    // (255 max, mas so usaremos, 14(0000 1110) no maximo do maximo)
 
     char tipo;          // 1 byte, nao sei como isso deve ficar
 
@@ -22,12 +20,12 @@ typedef struct mensagem {
 #define MEN_COMPRANDO_CARTA     5   // DONE
 #define MEN_RODADA_ACABOU       6   // DONE
 #define MEN_BASTAO              7   // DONE
-#define MEN_CONECCAO            8   //
+#define MEN_CONEXAO             8   // DONE
 #define MEN_JOGO_INI            9   // DONE
 
 
 // envia uma mensagem
-void enviar_mensagem(char tipo, char origem, int confirmacao, int conteudo);
+void enviar_mensagem(char tipo, char origem, int confirmacao, char conteudo1, char conteudo2);
 
 
 // recebe 1 mensagem e trata ela
