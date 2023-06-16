70a1bde7023615ae134b0f9644a3dee32eaf70cc
loi777 <35152365+loi777@users.noreply.github.com>
2023-06-16 04:14:13 -0300

    Update maquinas.c

--- SourceCode/maquinas.c
+++ SourceCode/maquinas.c
@@ -9,7 +9,11 @@
 //============================================
 
 
-maquina* computador;
+
+maquina computador;
+
+extern estado_jogo jogo;
+
 
 
 //============================================
@@ -30,22 +34,22 @@ void ler_setup() {
     //
 
     // obtem quantas maquinas teremos no total
-    fscanf(file, "%d", computador->qtd_maquinas);
+    fscanf(file, "%d", computador.qtd_maquinas);
 
     //
 
     // alloca o espaco para todos os ips
-    computador->todos_ips = malloc(computador->qtd_maquinas * sizeof(char*));   // qtd de ips
-    for (int i = 0; i < computador->qtd_maquinas; i++) {
+    computador.todos_ips = malloc(computador.qtd_maquinas * sizeof(char*));   // qtd de ips
+    for (int i = 0; i < computador.qtd_maquinas; i++) {
         // 14 eh o numero de chars que um ip precisa +1 para determinar o fim da string
-        computador->todos_ips[i] = malloc(14 * sizeof(char));
+        computador.todos_ips[i] = malloc(14 * sizeof(char));
     }
 
     //
 
     // obtem os ips salvos em setup.txt
-    for (int i = 0; i < computador->qtd_maquinas; i++) {
-        fgets(computador->todos_ips[i], 14, file);
+    for (int i = 0; i < computador.qtd_maquinas; i++) {
+        fgets(computador.todos_ips[i], 14, file);
     }
 
 }
@@ -64,9 +68,15 @@ void obtem_ip() {
 
 // tendo o ip, obtem a posicao de id dessa maquina na lista setup.txt
 void obtem_id() {
-    for (int i = 0; i < computador->qtd_maquinas; i++) {
-        if (computador->ip == computador->todos_ips[i]) {
-            computador->id = i;
+    for (int i = 0; i < computador.qtd_maquinas; i++) {
+        if (computador.ip == computador.todos_ips[i]) {
+            computador.id = i;
+
+            // sendo o primeiro id ele recebe o bastão
+            if (computador.id == 0) {
+                jogo.bastao = 1;
+            }
+
             return;
         }
     }
@@ -87,34 +97,30 @@ void obtem_id() {
 // tanto com o anterior quanto o proximo
 void coneccao_com_vizinhos() {
 
-    int prox_id = (computador->id + 1) % computador->qtd_maquinas;
+    computador.id_next = (computador.id + 1) % computador.qtd_maquinas;
 
-    int ante_id = (computador->id - 1);
-    if (ante_id < 0) {
-        ante_id = computador->qtd_maquinas + ante_id;
-    }
-
-    //
-
-    if (prox_id == computador->id) {
+    if (computador.id_next == computador.id) {
         fprintf(stderr, "ERRO: id do proximo eh igual o atual");
         return;
     }
 
-    if (ante_id == computador->id) {
-        fprintf(stderr, "ERRO: id do anterior eh igual o atual");
-        return;
-    }
-
     //
-
-    computador->ip_next = computador->todos_ips[prox_id];
 
     //computador->socket_next = obtem o socket da conecao
 
     //
 
-    computador->ip_prev = computador->todos_ips[ante_id];
+    computador.id_prev = (computador.id - 1);
+    if (computador.id_prev < 0) {
+        computador.id_prev += computador.qtd_maquinas;
+    }
+
+    if (computador.id_prev == computador.id) {
+        fprintf(stderr, "ERRO: id do anterior eh igual o atual");
+        return;
+    }
+
+    //
 
     //computador->socket_next = obtem o socket da conecao
 
